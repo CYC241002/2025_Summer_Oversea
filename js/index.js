2f00c363479f21fc5d0fc75de3ea9a31d463aa1c
DESKTOP-NDBTDHL\WildDagger <hoyunxian@gmail.com>
2025-03-18 10:38:22 +0800

    修正版面、增加景點連結

--- js/index.js
+++ js/index.js
@@ -17,6 +17,9 @@
             overlay.style.display = 'none'
         }, 1000);
 
+        const slogan = document.querySelector('header.header .slogan')
+        slogan.classList.remove('start')
+
         setTimeout(() => {
             if (window.location.hash) {
                 const target = document.querySelector(`a[name='${window.location.hash.replace('#','')}']`)
